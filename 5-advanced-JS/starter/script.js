6d37f7ee5998b6b6e7d394808b0f366b803f8b2d
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-22 18:38:28 -0400

    Created an object
    - created object using prototype.create using the second param to add extra values

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,11 +1,34 @@
-var Person = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    this.calculateAge = function() {
+//var Person = function(name, yearOfBirth, job) {
+//    this.name = name;
+//    this.yearOfBirth = yearOfBirth;
+//    this.job = job;
+//    this.calculateAge = function() {
+//        console.log(2019 - this.yearOfBirth);
+//    }
+//};
+//
+//var john = new Person('John', 1990, 'Teacher');
+//john.calculateAge();
+//
+
+// Object.create
+
+var personProto = {
+    calculateAge: function() {
         console.log(2019 - this.yearOfBirth);
     }
 };
 
-var john = new Person('John', 1990, 'Teacher');
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'Teacher';
 john.calculateAge();
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969 },
+    job: { value: 'deesigner'}
+});
+
+jane.calculateAge();
