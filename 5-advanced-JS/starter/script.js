32ed95d16f84d562cd4b153f23676b18434f6d5b
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-22 19:32:48 -0400

    Called functions using bind and call
    - used the object function call and bind to change the 'this' param thats passed into a funciton owned by another object

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -42,8 +42,32 @@
 //const job = 'Manager';
 //var quest = interviewQuestion(job);
 //quest(job);
+//
+//(function(goodLuck) {
+//    var score = Math.random() * 10;
+//    console.log(score >= (5 - goodLuck));
+//})(3);
 
-(function(goodLuck) {
-    var score = Math.random() * 10;
-    console.log(score >= (5 - goodLuck));
-})(3);
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if(style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlement! I\'m ' + this. name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');   
+       } else if(style === 'friendly') {
+           console.log('Hey! What\'s up? I\'m ' + this. name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+       }
+    }
+}
+
+john.presentation('friendly', 'evening');
+
+var emily = {
+    name: 'Emily',
+    age: 30,
+    job: 'designer'
+};
+
+john.presentation.call(emily, 'formal', 'morning');
+var johnFriendly = john.presentation.bind(john, 'friendly')();
