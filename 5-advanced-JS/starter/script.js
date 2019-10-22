d2b5121fa1aa1c511d177b25fafdba532c65015f
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-22 18:47:17 -0400

    Created a funcction
    - created a function to return another function to be user later

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -13,22 +13,32 @@
 
 // Object.create
 
-var personProto = {
-    calculateAge: function() {
-        console.log(2019 - this.yearOfBirth);
+//var personProto = {
+//    calculateAge: function() {
+//        console.log(2019 - this.yearOfBirth);
+//    }
+//};
+//
+//var john = Object.create(personProto);
+//john.name = 'John';
+//john.yearOfBirth = 1990;
+//john.job = 'Teacher';
+//john.calculateAge();
+//
+//var jane = Object.create(personProto, {
+//    name: { value: 'Jane'},
+//    yearOfBirth: { value: 1969 },
+//    job: { value: 'deesigner'}
+//});
+//
+//jane.calculateAge();
+
+function interviewQuestion(job) {
+    return function(job) {
+        console.log('What does being a ' + job + ' entail?');
     }
-};
+}
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'Teacher';
-john.calculateAge();
-
-var jane = Object.create(personProto, {
-    name: { value: 'Jane'},
-    yearOfBirth: { value: 1969 },
-    job: { value: 'deesigner'}
-});
-
-jane.calculateAge();
+const job = 'Manager';
+var quest = interviewQuestion(job);
+quest(job);
