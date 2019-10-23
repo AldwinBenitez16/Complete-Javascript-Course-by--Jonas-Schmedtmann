d4bfa7d2dc644abcfddb5eaa0cc7d3c3a8820d63
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-23 10:10:24 -0400

    Created get functions and DOMstring data structure
    - getFunctions get the UI values
    - DOMstrings hold the strings that define the different selectors for specific DOM elements

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -6,18 +6,43 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDomstrings: function() {
+            return DOMstrings;
+        }
+    }
+    
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        
-    });
+    var DOM = UICtrl.getDomstrings();
+    
+    var ctrlAddItem = function() {
+        var input = UICtrl.getinput();
+        console.log(input);
+    }
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(e) {
         if(e.keyCode === 13 || event.which === 13) {
-           console.log('Enter was pressed.');
+            ctrlAddItem();
            }
     });
     
