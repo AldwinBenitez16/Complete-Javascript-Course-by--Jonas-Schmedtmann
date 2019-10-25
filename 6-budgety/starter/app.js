503425c38ce3fbe4d81d418486da9659ced2e18f
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-24 22:32:13 -0400

    created budget functions
    - allows the data to be updated to contain the budget on item creation

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,13 +5,22 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
     
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
+    
+    var calculateTotal = function(type) {
+        var sum = 0;
+        
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
     
     var data = {
         allItems: {
@@ -21,8 +30,10 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
-    }
+        },
+        budget: 0,
+        percentage: -1
+    };
     
     return {
         addItem: function(type, des, val) {
@@ -43,6 +54,30 @@ var budgetController = (function() {
             data.allItems[type].push(newItem);
             return newItem;
         },
+        
+        calculateBudget: function() {
+            
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            data.budget = data.totals.inc - data.totals.exp;
+
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+        
         testing: function() {
             console.log(data);
         }
@@ -127,20 +162,29 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
     
     var updateBudget = function() {
+        var budget;
+        
+        budgetCtrl.calculateBudget();
+        
+        budget = budgetCtrl.getBudget();
+        
+        console.log(budget);
         
     };
     
     var ctrlAddItem = function() {
         var input, newItem;
         
+        input = UICtrl.getinput();
+        
         if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
-            input = UICtrl.getinput();
-
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
             UICtrl.addListItem(newItem, input.type);
 
             UICtrl.clearFields();
+            
+            updateBudget();
         }
     };
     
