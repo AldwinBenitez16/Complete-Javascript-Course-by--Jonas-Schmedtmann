e872b2f8cf213ce73564f6bcb3293ba0a0bb75d2
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-25 23:29:01 -0400

    Using ES6 new string methods
    - startWith, includes, and endWith return a boolean which specifies the method inquiry
    - string.repeat(num) repeats a string

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,9 +1,27 @@
 // Blocks and IIFEs
 
-{
-    const a = 3;
-    let b = 2;
-    var c = 5;
+//{
+//    const a = 3;
+//    let b = 2;
+//    var c = 5;
+//}
+//
+//console.log(c);
+
+// Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2019 - year;
 }
 
-console.log(c);
+// ES6
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('th'));
+console.log(n.includes(' '));
+console.log(firstName.repeat(5));
