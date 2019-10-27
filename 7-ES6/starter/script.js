599fb5d623684f5679dad4d8e3d9362e56cff839
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-26 21:54:05 -0400

    Used ES6 Destructuring
    - created individual const variables to contain values in array
    - got values in obj using destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -22,22 +22,62 @@
 //
 //const n = `${firstName} ${lastName}`;
 //console.log(n.startsWith('j'));
-//console.log(n.endsWith('th'));
+//console.log(n.endsWith('th')); 
 //console.log(n.includes(' '));
 //console.log(firstName.repeat(5));
 
 //ES6
 
-const box6 = {
-    color: 'green',
-    position: 1,
-    clickMe: function() {
-        
-        document.querySelector('.green').addEventListener('click', () => {
-            
-            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-            alert(str);
-        });
-    }
-}
-box6.clickMe();
+//const box6 = {
+//    color: 'green',
+//    position: 1,
+//    clickMe: () => {
+//        
+//        document.querySelector('.green').addEventListener('click', () => {
+//            
+//            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//            alert(str);
+//        });
+//    }
+//}
+//box6.clickMe();
+
+// ES6
+
+//function Person(name) {
+//    this.name = name;
+//}
+//
+//Person.prototype.myFriends5 = function(friends) {
+//    
+//    var arr = friends.map(function(el) {
+//        return this.name + ' is friends with ' + el;
+//    }.bind(this));
+//    console.log(arr);
+//};
+//
+//var friends = ['Bob', 'Jane', 'Mark'];
+//new Person('John').myFriends5(friends);
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const { firstName, lastName } = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+
+console.log(a);
+console.log(b);
+
+
+
+
