70a622e4d5e4b4bd14a499928a2e91ab1d2f1818
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-26 23:23:40 -0400

    Using ES6 Map data structure
     - Created a map
    - set values using strings, number, boolean keys
    - retrieved map values using map.get()
    - checked if key exist wiht map.has()

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -80,8 +80,41 @@
 
 // ES6
 
-const h = document.querySelector('h1');
-const boxes = document.querySelectorAll(.box);
-const all = [h, ...boxes];
+//const h = document.querySelector('h1');
+//const boxes = document.querySelectorAll(.box);
+//const all = [h, ...boxes];
+//
+//Array.from(all).forEach(cur => cur.style.color = 'purple');
 
-Array.from(all).forEach(cur => cur.style.color = 'purple');
+// Maps
+
+const question = new Map();
+
+question.set('question', 'What is the official name of the latest major JavasScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct Answer');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get('question'));
+
+if(question.has(4)) {
+//    question.delete(4);
+    console.log('Answer 4 is here');
+}
+
+//question.clear();
+
+//question.forEach((value, key) => {
+//    console.log(`This is ${key}, and it's set to ${value}`);
+//});
+
+//for(let [key, value] of question.entries()) {
+//    
+//}
+
+
+
