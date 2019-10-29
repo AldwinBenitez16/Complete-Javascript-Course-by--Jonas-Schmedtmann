70c8f5a0c9ddb28dea563f7f2eb33f22c96e640b
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-29 19:25:25 -0400

    added webpack-server

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -6,8 +6,10 @@ module.exports = {
     entry: './src/js/index.js',
     // state the absolute path and connect it to new folder where file will rest in
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js' // specifies the file name
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js' // specifies the file name
     },
-    mode: 'development'
+    devServer: {
+        contentBase: './dist'
+    }
 }
